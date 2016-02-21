6470779484654a86015bc9e97b96f5f6bb930636
garypy <imblues@126.com>
2016-02-21 23:01:52 +0800

    Initialize template, static file and urls, views class

--- BBS/app/views.py
+++ BBS/app/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+
+def index(request):
+    return render_to_response('bbs/index.html')
