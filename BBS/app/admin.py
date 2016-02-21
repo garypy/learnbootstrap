784dd50f0876452d5cf5a3923d4a162ef133d0ec
garypy <imblues@126.com>
2016-02-21 21:59:08 +0800

    Show three tables in Admin, and display some columns in the BBS table. Set the encoding zh-CN to show Chinese UI in the admin

---
+++ BBS/app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from app import models
+
+class BBS_admin(admin.ModelAdmin):
+    list_display = ('title', 'summary', 'author', 'view_count', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('title', 'author__user__username')
+
+admin.site.register(models.BBS, BBS_admin)
+admin.site.register(models.Category)
+admin.site.register(models.BBS_user)
+
--- BBS/app/models.py
+++ BBS/app/models.py
@@ -23,10 +23,13 @@ class Category(models.Model):
     name = models.CharField(max_length=32, unique=True)
     administrator = models.ForeignKey('BBS_user')
 
+    def __unicode__(self):
+        return self.name
+
 class BBS_user(models.Model):
     user = models.OneToOneField(User)
     signature = models.CharField(max_length=128, default='This guy is too lazy to leave anything here.')
     photo = models.ImageField(upload_to="upload_images/", default="upload_imags/user-1.jpg")
 
     def __unicode__(self):
-        return self.user
+        return self.user.username
